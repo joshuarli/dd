4a55f39736578404471312ffe8d640b0b1fd40fa
Joshua Li <josh@jrl.ninja>
2019-03-31 20:45:29 -0400

    add some more sofware packages

--- install-stage2.sh
+++ install-stage2.sh
@@ -20,6 +20,23 @@ sudo systemctl enable /etc/systemd/system/physlock.service
 
 printf %s\\n "installing other good software..."
 yay --noconfirm --needed -S \
-    dfc ncdu htop \
-    editorconfig-core-c micro-bin nano \
-    mpv-git
+    dfc ncdu htop ripgrep fd exa rsync jq atool binutils upx qemu \
+    inotify-tools android-tools firejail wget aria2 \
+    strace ltrace bind-tools \
+    editorconfig-core-c micro-bin nano screen tmux tio \
+    pass rofi-pass \
+    inxi acpi lshw \
+    mpv-git pavucontrol-qt cava \
+    irssi qbittorrent \
+    grabc \
+    qpdfview gpicview leafpad
+
+# fonts
+#   firefox default serif + sans-serif: ttf-liberation
+#   east asian: adobe-source-han-sans-otc-fonts
+
+# other stuff i use that requires more manual/involved installs
+#   - localepurge
+#   - dnscrypt-proxy
+#   - joshuarli-spacefm
+#   - tlp + powertop
